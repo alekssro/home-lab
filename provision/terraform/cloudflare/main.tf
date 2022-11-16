9af33a20a4abf2b471813a375f1bd603f5cd7920
alekssro <alekss.ro@gmail.com>
2022-11-16 02:34:07 +0100

    dns: Add VPN record

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -67,7 +67,16 @@ resource "cloudflare_record" "root" {
   value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
   proxied = true
   type    = "CNAME"
-  ttl     = 1
+  ttl     = 30
+}
+
+resource "cloudflare_record" "vpn" {
+  name    = "vpn"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  proxied = true
+  type    = "CNAME"
+  ttl     = 30
 }
 
 # Removed from tf state (managed by cloudflare-ddns cronjob in k8s cluster)
