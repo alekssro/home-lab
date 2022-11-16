65978e744bbc504dd2205cd4be70e61eb838a82d
alekssro <alekss.ro@gmail.com>
2022-11-16 02:53:28 +0100

    fix: Minumum allowed TTL

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -76,7 +76,7 @@ resource "cloudflare_record" "vpn" {
   value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
   proxied = false
   type    = "CNAME"
-  ttl     = 30
+  ttl     = 60
 }
 
 # Removed from tf state (managed by cloudflare-ddns cronjob in k8s cluster)
